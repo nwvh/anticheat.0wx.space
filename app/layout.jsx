d08655d39051512624603a4bd0e48869d636111a
wx <76164598+nwvh@users.noreply.github.com>
2023-12-21 20:08:00 +0100

    feat(web): added embeds for discord

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Montserrat } from "next/font/google";
+import Head from 'next/head'; // Import the Head component
 import "./globals.css";
 import { Providers } from "./providers";
 
@@ -12,6 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta content="WX AntiCheat" property="og:title" />
+        <meta content='"The only anticheat you will ever need". The official website for WX AntiCheat, an advanced and affordable FiveM AntiCheat." property="og:description' />
+        <meta content="https://anticheat.0wx.space" property="og:url" />
+        <meta content="https://media.discordapp.net/attachments/1134103622423154751/1187454603420246096/wxaclogo.png" property="og:image" />
+        <meta content="#C4B5FD" data-react-helmet="true" name="theme-color" />
+      </Head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
